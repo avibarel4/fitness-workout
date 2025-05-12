d7f5269431d9d6b7643284acaac2c21db46135a5
Avi Barel <avi.barel@bluevine.com>
2025-05-12 10:49:35 +0300

    Colors Infra

---
+++ app/src/main/java/com/avi/fitnessworkout/ui/theme/FWColor.kt
@@ -0,0 +1,12 @@
+package com.avi.fitnessworkout.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object FWColor {
+    val Black = Color(0xFF000000)
+    val White = Color(0xFFFFFFFF)
+    val GeneralBackground = Color(0xFF14171D)
+    val LightGray = Color(0xFF465773)
+    val Yellow = Color(0xFFFCD90D)
+    val LightBlue = Color(0xFF3995F1)
+}
--- app/src/main/java/com/avi/fitnessworkout/ui/theme/Type.kt
+++ app/src/main/java/com/avi/fitnessworkout/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.avi.fitnessworkout.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,8 +15,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -29,6 +29,33 @@ val Typography = Typography(
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
